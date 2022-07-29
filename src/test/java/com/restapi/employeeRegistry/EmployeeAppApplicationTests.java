192513bea5d420d106f7ad812cfc24b1f24f6ad6
Adhithya1999 <anirudhadhithya20@gmail.com>
2022-07-29 16:24:36 +0530

    first commit

---
+++ src/test/java/com/restapi/employeeRegistry/EmployeeAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.restapi.employeeRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+@ContextConfiguration
+@SpringBootTest
+class EmployeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
